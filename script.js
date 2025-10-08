da9cb771c1f668cf67e4a5c1bec07f20ce6d3761
Seygo0708 <seygocastroperez@gmail.com>
2025-10-08 00:04:52 -0500

    Subiendo carpeta ALMACEN con archivos actualizados

--- script.js
+++ script.js
@@ -888,7 +888,7 @@ async function exportarExcel() {
             const item = doc.data();
             return {
                 'Fecha Ãšltima ActualizaciÃ³n': item.fechaActualizacion || 'N/A',
-                CÃ³digo: item.codigo,
+                CÃdigo: item.codigo,
                 Nombre: item.nombre,
                 Lote: item.lote || '',
                 'Costo Unitario (S/.)': parseFloat(item.costoUnitario).toFixed(2),
